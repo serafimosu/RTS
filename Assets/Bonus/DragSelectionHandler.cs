6829d28996f47a9b984e94d689273b3c0e6ff38d
serafimosu <bacaluserafim@gmail.com>
2023-11-25 14:54:14 +0200

    unit

---
+++ Assets/Bonus/DragSelectionHandler.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class DragSelectionHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
+{
+    public Image SelectionBoxImage;
+    Vector2 StartPos;
+    Rect SelectionRect;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
+        {
+            MySelectable.DeselectAll(new BaseEventData(EventSystem.current));
+        }
+        SelectionBoxImage.gameObject.SetActive(true);
+        StartPos = eventData.position;
+        SelectionRect = new Rect();
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // X axis
+        if (eventData.position.x < StartPos.x)
+        {
+            SelectionRect.xMin = eventData.position.x;
+            SelectionRect.xMax = StartPos.x;
+        }
+        else
+        {
+            SelectionRect.xMin = StartPos.x;
+            SelectionRect.xMax = eventData.position.x;
+        }
+        // Y axis 
+        if (eventData.position.y < StartPos.y)
+        {
+            SelectionRect.yMin = eventData.position.y;
+            SelectionRect.yMax = StartPos.y;
+        }
+        else
+        {
+            SelectionRect.yMin = StartPos.y;
+            SelectionRect.yMax = eventData.position.y;
+        }
+
+        SelectionBoxImage.rectTransform.offsetMin = SelectionRect.min;
+        SelectionBoxImage.rectTransform.offsetMax = SelectionRect.max;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        SelectionBoxImage.gameObject.SetActive(false);
+        foreach (MySelectable selected in MySelectable.allMySelectables)
+        {
+            if (SelectionRect.Contains(Camera.main.WorldToScreenPoint(selected.transform.position)))
+            {
+                selected.OnSelect(eventData);
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        float myDistance = 0;
+        foreach (RaycastResult item in results)
+        {
+            if (item.gameObject == gameObject)
+            {
+                myDistance = item.distance;
+                break;
+            }
+        }
+        GameObject nextObject = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (RaycastResult item in results)
+        {
+            if (item.distance > myDistance && item.distance < maxDistance)
+            {
+                nextObject = item.gameObject;
+                maxDistance = item.distance;
+            }
+        }
+        if (nextObject)
+        {
+            ExecuteEvents.Execute<IPointerClickHandler>(nextObject, eventData, (x, y) => { x.OnPointerClick((PointerEventData)y); });
+        }
+    }
+}
---
+++ Assets/Bonus/MySelectable.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+
+public class MySelectable : MonoBehaviour, ISelectHandler, IPointerClickHandler, IDeselectHandler
+{
+    //saját egységek, az összes, és a kijelöltek közülük.
+    public static List<MySelectable> allMySelectables = new List<MySelectable>();
+    public static List<MySelectable> currentlySelected = new List<MySelectable>();
+
+    public Material unSelectedMat;
+
+    public static Material SelectedMat;
+    public Material selectedMatFor;
+
+    private ParticleSystem myParticle;
+    private SkinnedMeshRenderer smr;
+
+    public bool selected;
+    private void Awake()
+    {
+        //static ellen cheat
+        SelectedMat = selectedMatFor;
+
+        //belerakom az össz saját objektumot egy listába
+        allMySelectables.Add(this);
+
+        //objektum rendererje
+        smr = GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //control gombbal pluszban többet jelöl ki,
+        if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
+        {
+            DeselectAll(eventData);
+        }
+        //anélkül csak egyet.
+        OnSelect(eventData);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        //jelenleg ezt jelöltük ki, aki nem Ellenfél
+        if (gameObject.tag != "Enemy")
+        {
+            currentlySelected.Add(this);
+            selected = true;
+            //máskép nézki, jelölés látszódik.
+            smr.material = SelectedMat;
+        }
+    }
+    public void OnDeselect(BaseEventData eventData)
+    {
+        //anyag vissza állítása.
+        smr.material = unSelectedMat;
+        selected = false;
+    }
+    public static void DeselectAll(BaseEventData eventData)
+    {
+        //Az összes eddig kijelölt elemet jelöletlenné állítjuk.
+
+        foreach (MySelectable unit in currentlySelected)
+        {
+            unit.OnDeselect(eventData);
+        }
+        //utána tisztítjuk a jelenlegi kijelölt listát.
+        currentlySelected.Clear();
+    }
+}
